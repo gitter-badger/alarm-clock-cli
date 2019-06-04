dbeafa3e678314db2a877997680f35c4287d5cde
Carlos Rafael de Oliveira Carneiro <carlos_rafael_c.r.o.c@live.com>
2019-06-04 01:14:45 -0300

    We already can set an alarm clock to play any music. - Without persistence. - Data in memory. - Have to work as a service. - Commands have to persist data.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,93 @@
 # include<stdio.h>
+# include<stdlib.h>
+# include<string.h>
+# include<time.h>
+# include<getopt.h>
+# include<unistd.h>
+
+enum operations 
+{
+	ADD
+};
+
+struct Options
+{
+	int op;
+	char *at;
+	char *name;
+	char *plays;
+};
+
+struct Options *parseArguments(int argc, char *argv[])
+{
+	struct Options *options = (struct options*) malloc(sizeof(struct Options));
+	int c;
+
+	static struct option long_options[] = 
+	{
+		{ "add", no_argument, 0, 'a' },
+		{ "at", required_argument, 0, 't'},
+		{ "name", required_argument, 0, 'n'},
+		{ "plays", required_argument, 0, 'p'},
+		{0, 0, 0, 0}
+	};
+
+	int option_index = 0;
+
+	while((c = getopt_long(argc, argv, "ft:", long_options, &option_index)) != -1)
+	{
+		switch (c)
+		{
+		case 'a':
+			options->op = ADD;
+			break;
+		case 't':
+			options->at = optarg;
+			break;
+		case 'n':
+			options->name = optarg;
+			break;
+		case 'p':
+			options->plays = optarg;
+			break;
+		default:
+			abort();
+		}
+	}
+
+	return options;
+}
 
 int main(int argc, char *argv[])
 {
-	puts("Hello World!");
+	const char *playMusicCommandTemplate = "rhythmbox \"%s\"";
+	struct Options *options = parseArguments(argc, argv);
+	char *playMusicCommand = (char*) malloc(sizeof(char) * (strlen(options->plays) + strlen(playMusicCommandTemplate)));	
+	int alarmHour, alarmMin;
+
+	sscanf(options->at, "%d:%d", &alarmHour, &alarmMin);
+
+	printf("\n%s\n%s\n%s\n", options->name, options->at, options->plays);
+
+	do {
+		time_t rawtime;
+		struct tm *timeinfo;
+		time(&rawtime);
+		timeinfo = localtime(&rawtime);
+		printf("%0.2d\n", timeinfo->tm_hour);
+		printf("%0.2d\n", timeinfo->tm_min);
+
+		if(timeinfo->tm_hour == alarmHour && timeinfo->tm_min == alarmMin)
+		{
+			sprintf(playMusicCommand, playMusicCommandTemplate, options->plays);
+			printf("Executing command: %s\n\n", playMusicCommand);
+			system(playMusicCommand);
+
+			return 0;
+		}
+
+		sleep(60);
+	} while(1);
+
 	return 0;
 }
